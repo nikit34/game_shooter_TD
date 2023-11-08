f5a62a1c4ae06c132f3b60a5948bf61750eca942
nikit34 <permikov134@mail.ru>
2023-11-08 00:29:01 +0000

    Change bullets

--- settings.py
+++ settings.py
@@ -8,9 +8,9 @@ class Settings():
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
         self.ship_limit = 3
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_width = 5
+        self.bullet_height = 20
+        self.bullet_color = 255, 0, 0
         self.bullets_allowed = 3
         self.fleet_drop_speed = 20
         self.speedup_scale = 1.3
